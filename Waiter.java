150c84e9e6384b2e99548315e710ad4a5a9cf1e4
Chris Litting <mr_litting@hotmail.com>
2022-03-25 16:59:44 +0000

    Added methods wireframe to waiter class

--- Waiter.java
+++ Waiter.java
@@ -1,3 +1,19 @@
 public class Waiter extends Staff {
     
+    public void newEatInOrder() {
+        //add code here
+    }
+
+    public void cancelOrder() {
+        //add code here
+    }
+
+    public void approveBooking() {
+        //add code here
+    }
+
+    public void approveDelivery() {
+        //add code here
+    }
+    
 }
