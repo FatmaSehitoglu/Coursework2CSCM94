a2852e0c7c994c2afc8f58d9993b4d72f1e3184d
Chris Litting <mr_litting@hotmail.com>
2022-03-25 22:27:35 +0000

    Added constructor for Chef class

--- Chef.java
+++ Chef.java
@@ -1,3 +1,10 @@
 public class Chef extends Staff {
-    
+
+     //Constructor method
+     public Chef(String firstName, String lastName, int staffId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.staffId = staffId;
+    }
+
 }
