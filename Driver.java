754a250fbf51638346929520072a79c0fb9754a3
Chris Litting <mr_litting@hotmail.com>
2022-03-25 22:28:40 +0000

    Added constructor for Driver class

--- Driver.java
+++ Driver.java
@@ -1,3 +1,10 @@
 public class Driver extends Staff {
-    
+
+     //Constructor method
+     public Driver(String firstName, String lastName, int staffId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.staffId = staffId;
+    }
+
 }
