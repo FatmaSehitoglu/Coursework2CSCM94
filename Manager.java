fded20bb9672b5f129c6485f10b515f7472cdc27
Chris Litting <mr_litting@hotmail.com>
2022-03-25 22:28:11 +0000

    Minor change to constructor method

--- Manager.java
+++ Manager.java
@@ -2,9 +2,9 @@ public class Manager extends Staff {
 
     //Constructor method
     public Manager(String firstName, String lastName, int staffId) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.staffId = staffId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.staffId = staffId;
     }
 
 }
