df943e9bfa764929a9c0f93e99131facbbea3197
Chris Litting <mr_litting@hotmail.com>
2022-03-25 22:26:49 +0000

    Added constructor for Manager class

--- Manager.java
+++ Manager.java
@@ -1,3 +1,10 @@
 public class Manager extends Staff {
-    
+
+    //Constructor method
+    public Manager(String firstName, String lastName, int staffId) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.staffId = staffId;
+    }
+
 }
