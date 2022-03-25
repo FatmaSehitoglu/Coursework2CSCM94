2ee8be7c78cd3dd519cc0496dfa188e2f75b068e
Chris Litting <mr_litting@hotmail.com>
2022-03-25 22:35:16 +0000

    Added constructor for Customer class

--- Customer.java
+++ Customer.java
@@ -1,7 +1,14 @@
-public class Customer extends User{
+public class Customer extends User {
     String address;
     int custId;
     Order[] orderHistory;
+
+    public Customer(String firstName, String lastName, String address, int custId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.custId = custId;
+    }
 
     //public void createNewEvent(){} (not implemented)
     //public void viewEvents(){} (not implemented)
