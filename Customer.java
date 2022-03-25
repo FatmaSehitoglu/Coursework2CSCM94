02167a76225989cdca5a8a78f856c2b2c877d9c0
Chris Litting <mr_litting@hotmail.com>
2022-03-25 16:57:16 +0000

    Added methods wireframe to Customer class

--- Customer.java
+++ Customer.java
@@ -2,4 +2,25 @@ public class Customer extends User{
     String address;
     int custId;
     Order[] orderHistory;
+
+    //public void createNewEvent(){} (not implemented)
+    //public void viewEvents(){} (not implemented)
+    //public void joinEvent() {} (not implemented)
+
+    public void requestBooking() {
+        //add code here
+    }
+
+    public void cancelBooking() {
+        //add code here
+    }
+
+    public void placeDeliveryOrder() {
+        //add code here
+    }
+
+    public void placeTakeAwayOrder() {
+        //add code here
+    }
+
 }
