7f806fa3517009fa6feac7d4e8ab24b696ccc716
NzyZzz1998 <101812716+NzyZzz1998@users.noreply.github.com>
2022-03-26 17:41:19 +0000

    Update Takeaway.java

--- Takeaway.java
+++ Takeaway.java
@@ -1,19 +1,14 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class Takeaway extends Order{
     private LocalDateTime arriveTime;
 
-    /*public Takeaway(int customID, String[] items, boolean orderComplete, LocalDateTime time, LocalDateTime arriveTime) {
-        super(customID, items, orderComplete, time);
-        this.arriveTime = arriveTime;
-    }*/
-
-    public Takeaway(LocalDateTime arriveTime, ArrayList<MenuItem> items) {
-        this.arriveTime = arriveTime;
-        record(items);
+    public Takeaway(String customID, ArrayList<MenuItem> items, boolean approval, LocalDateTime time) {
+        super(customID, items, approval);
     }
+
 
     public LocalDateTime getArriveTime() {
         return arriveTime;
@@ -30,15 +25,4 @@ public class Takeaway extends Order{
                 '}';
     }
 
-    @Override
-    public void record(ArrayList<MenuItem> items) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Please enter the customer ID:");
-        int id = sc.nextInt();
-        setCustomID(id);
-        setItems(items);
-        setTime(LocalDateTime.now());
-        setOrderComplete(false);
-    }
 }
