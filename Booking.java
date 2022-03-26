b18eff6d7c057b76f6fb60c67a22f63f9bf5344b
Martin Dimitrov <martivr98@gmail.com>
2022-03-26 01:33:30 +0000

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -1,3 +1,49 @@
+import java.util.ArrayList;
 public class Booking {
-    
+    private String date;
+    private String time;
+    private int customerID;
+    private boolean approved;
+    public void addBooking(Booking book){
+        bookings.add(book);
+    }
+    ArrayList<Booking> bookings = new ArrayList<>();
+
+    public Booking(String date, String time, int customerID, boolean approved) {
+        this.date = date;
+        this.time = time;
+        this.customerID = customerID;
+        this.approved = approved;
+    }
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
 }
