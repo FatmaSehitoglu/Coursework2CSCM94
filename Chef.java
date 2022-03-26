3f6dbd7c9c001f74b1e9baaac606e3b74cd3fb7d
Chris Litting <mr_litting@hotmail.com>
2022-03-26 15:20:53 +0000

    Minor changes to Chef gets/sets

--- Chef.java
+++ Chef.java
@@ -16,28 +16,10 @@ public class Chef extends Staff {
 
     //SETTERS
     //=======
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
+   
     //GETTERS
     //=======
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public int getStaffId() {
-        return this.staffId;
-    }
-
+   
     //METHODS
     //=======
     public void completeOrder() {
