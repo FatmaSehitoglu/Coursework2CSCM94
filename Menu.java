e39661848a8cd31cebaa52087a15725d096bd428
Martin Dimitrov <martivr98@gmail.com>
2022-03-26 01:11:47 +0000

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+
 public class Menu {
-    
+    private ArrayList<MenuItem>menuItems = new ArrayList<>();
+public void addMenuItem(MenuItem item){
+    menuItems.add(item);
+    }
 }
