68bec3f3fb9f018f761856cabb0a71492542c334
Chris Litting <mr_litting@hotmail.com>
2022-03-26 15:10:37 +0000

    Added Staff gets/sets

--- Staff.java
+++ Staff.java
@@ -1,5 +1,21 @@
 public abstract class Staff extends User{
+    
+    //FIELDS
+    //======
     //int totalHoursWorked; (not implemented)
     //Shift[] hoursToWork; (not implemented)
     int staffId;
+
+    //SETTERS
+    //=======
+    public void setStaffId(int staffId) {
+        this.staffId = staffId;
+    }
+
+    //GETTERS
+    //=======
+    public int getStaffId() {
+        return this.staffId;
+    }
+
 }
