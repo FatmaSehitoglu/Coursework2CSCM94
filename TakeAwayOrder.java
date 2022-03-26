d56d17ae78881ae1bb29212d9b351eb078a85cbb
Chris Litting <mr_litting@hotmail.com>
2022-03-26 13:53:19 +0000

    Added constructor for TakeAwayOrder

--- TakeAwayOrder.java
+++ TakeAwayOrder.java
@@ -1,3 +1,39 @@
+
+
 public class TakeAwayOrder {
     
+    //FIELDS
+    //======
+    //private int custId;           (inherited)
+    //private boolean completed;    (inherited)
+    //private String dateTime;      (inherited)
+    private int pickupTime;
+
+    //CONSTRUCTOR
+    //===========
+    public TakeAwayOrder(
+        int custId, 
+        boolean completed, 
+        int pickupTime
+    ) {
+        this.custId = custId;
+        this.completed = false;
+        this.dateTime = currentDateTime();
+        this.pickupTime = pickupTime;
+    }
+    
+    
+    //SETTERS
+    //=======
+
+
+
+    //METHODS
+    //=======
+    private static String currentDateTime() {
+        String dt = "";
+        return dt;
+    }
+
+
 }
