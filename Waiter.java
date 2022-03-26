91c98442ed62e66d52a134afd2915b246b00c14f
Chris Litting <mr_litting@hotmail.com>
2022-03-26 15:12:58 +0000

    Changes to Waiter gets/sets

--- Waiter.java
+++ Waiter.java
@@ -1,36 +1,26 @@
 public class Waiter extends Staff {
     
+    //FIELDS
+    //======
+    //private firstName     (inherited)
+    //private lastName      (inherited)
+    //private staffId       (inherited)
+
     //CONSTRUCTOR
     //===========
     public Waiter(String firstName, String lastName, int staffId) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.staffId = staffId;
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setStaffId(staffId);
     }
 
     //SETTERS
     //=======
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    
 
     //GETTERS
     //=======
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public int getStaffId() {
-        return this.staffId;
-    }
+   
 
     //METHODS
     //=======
