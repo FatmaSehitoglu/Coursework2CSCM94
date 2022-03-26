79dc82379c0d7f927e91495a6870113ef207e7e1
Chris Litting <mr_litting@hotmail.com>
2022-03-26 15:21:42 +0000

    Changes to Driver gets/sets

--- Driver.java
+++ Driver.java
@@ -1,36 +1,24 @@
 public class Driver extends Staff {
 
-     //CONSTRUCTOR
-     //===========
-     public Driver(String firstName, String lastName, int staffId) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.staffId = staffId;
+    //FIELDS
+    //======
+    //private firstName     (inherited)
+    //private lastName      (inherited)
+    //private staffId       (inherited)    
+    
+    //CONSTRUCTOR
+    //===========
+    public Driver(String firstName, String lastName, int staffId) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setStaffId(staffId);
     }
 
     //SETTERS
     //=======
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
+   
     //GETTERS
     //=======
-    public String getFirstName() {
-        return this.firstName;
-    }
-
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public int getStaffId() {
-        return this.staffId;
-    }
 
     //METHODS
     //=======
