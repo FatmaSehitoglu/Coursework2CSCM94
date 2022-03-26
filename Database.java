7d494a0309eb0f655f9e89be28f4b6c041c78bdb
Chris Litting <mr_litting@hotmail.com>
2022-03-26 14:25:06 +0000

    Added minor comments to Database

--- Database.java
+++ Database.java
@@ -1,9 +1,13 @@
 public class Database {
 
+    //FIELDS
+    //======
     public static Order[] orders;
     public static Booking[] bookings;
     public static Customer[] customers;
     public static Staff[] staff;
     public static MenuItem[] menuItems;
+
+    
 
 }
