31ccfc00b5cf69db70e56d383573bc39ab22ba23
Chris Litting <mr_litting@hotmail.com>
2022-03-26 13:33:34 +0000

    Added gets/sets and comments to Chef

--- Chef.java
+++ Chef.java
@@ -1,12 +1,39 @@
 public class Chef extends Staff {
 
-     //Constructor method
+     //CONSTRUCTOR
+     //===========
      public Chef(String firstName, String lastName, int staffId) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.staffId = staffId;
     }
 
+    //SETTERS
+    //=======
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    //GETTERS
+    //=======
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getStaffId() {
+        return this.staffId;
+    }
+
+    //METHODS
+    //=======
     public void completeOrder() {
         //add code here
     }
