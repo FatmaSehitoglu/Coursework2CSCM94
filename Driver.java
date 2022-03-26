f88999b3767f4feae92966488e8edab71b2b1598
Chris Litting <mr_litting@hotmail.com>
2022-03-26 13:34:35 +0000

    Added gets/sets and comments to Driver

--- Driver.java
+++ Driver.java
@@ -1,10 +1,42 @@
 public class Driver extends Staff {
 
-     //Constructor method
+     //CONSTRUCTOR
+     //===========
      public Driver(String firstName, String lastName, int staffId) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.staffId = staffId;
     }
+
+    //SETTERS
+    //=======
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    //GETTERS
+    //=======
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getStaffId() {
+        return this.staffId;
+    }
+
+    //METHODS
+    //=======
+
+    //Add code here
+
+
 
 }
