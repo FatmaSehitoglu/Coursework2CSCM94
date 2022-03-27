33712c2c4ece077bb1d8f4985aba9e3608326180
fdgod <martivr98@gmail.com>
2022-03-27 12:32:42 +0100

    dashg

--- Menu.java
+++ Menu.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Menu {
     MenuItem Pizza = new MenuItem(1,"Pepperoni", "Pizza");
     MenuItem Pasta = new MenuItem(2, "Carbonara", "Pasta");
-
+    MenuItem Chicken = new MenuItem(3, "Chicken tomato sauce", "Chicken");
     public static ArrayList<MenuItem>menuItems = new ArrayList<>();
     public static void addMenuItem(MenuItem item){
         menuItems.add(item);
