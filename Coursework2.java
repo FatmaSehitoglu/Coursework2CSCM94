f0ca712a1dd3372291b7e7ae6e1eed1292560334
Fatma Sehitoglu <fsehitoglu97@gmail.com>
2022-03-27 14:10:04 +0100

    booking

--- Coursework2.java
+++ Coursework2.java
@@ -2,23 +2,16 @@ import java.util.Scanner;
 //import java.util.ArrayList;
 class Coursework2 {
     public static void main(String[] args) {
-        /*
         System.out.println("Hello World!"); 
         Scanner in = new Scanner(System.in);
         Customer cust1 = new Customer("fatma", "sh", "Swansea", "00011");
-        //String x = in.nextLine();
-       // String y = in.nextLine();
-        cust1.requestBooking("7","4",3);
-        System.out.println(Database.getBookingHistory().get(0).getTime());
-        */
-
-        Manager george = new Manager("George", "Mallard", "george@mallard.com");
-        Database.addToStaffList(george);
-        User user = george;
-        user.displayMainMenu();
-
-
-
-
+        System.out.println("Enter the date for the booking");
+        String x = in.nextLine();
+        System.out.println("Enter the time for the booking");
+        String y = in.nextLine();
+        System.out.println("Enter the number of guests for the booking");
+        int guestNo = in.nextInt();
+        cust1.requestBooking(x,y,guestNo);
+        
     }
 }
