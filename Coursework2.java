bca2e66a7a97a0bff58451df7f6694dd0b15e9ab
Chris Litting <mr_litting@hotmail.com>
2022-03-27 14:07:05 +0100

    Experiment with Manager menus in Main

--- Coursework2.java
+++ Coursework2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 //import java.util.ArrayList;
 class Coursework2 {
     public static void main(String[] args) {
+        /*
         System.out.println("Hello World!"); 
         try (Scanner in = new Scanner(System.in)) {
         }
@@ -10,5 +11,15 @@ class Coursework2 {
        // String y = in.nextLine();
         cust1.requestBooking("7","4",3);
         System.out.println(Database.getBookingHistory().get(0).getTime());
+        */
+
+        Manager george = new Manager("George", "Mallard", "george@mallard.com");
+        Database.addToStaffList(george);
+        User user = george;
+        user.displayMainMenu();
+
+
+
+
     }
 }
