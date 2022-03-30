ecac0f09f61846230796feb3535d83b1555d49c4
NzyZzz1998 <101812716+NzyZzz1998@users.noreply.github.com>
2022-03-30 12:36:14 +0100

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,9 +1,10 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public abstract class Order {
     private String customID;
-    private ArrayList<MenuItem> items;
+    private ArrayList<MenuItem>[] items;
     private boolean orderComplete;
     private LocalDateTime time;
     private boolean chefComplete;
@@ -11,7 +12,7 @@ public abstract class Order {
 
 
 
-    public Order(String customID, ArrayList<MenuItem> items, boolean approval) {
+    public Order(String customID, ArrayList<MenuItem>[] items, boolean approval) {
         this.customID = customID;
         this.items = items;
         this.approval = approval;
@@ -30,11 +31,11 @@ public abstract class Order {
         this.customID = customID;
     }
 
-    public ArrayList<MenuItem> getItems() {
+    public ArrayList<MenuItem>[] getItems() {
         return items;
     }
 
-    public void setItems(ArrayList<MenuItem> items) {
+    public void setItems(ArrayList<MenuItem>[] items) {
         this.items = items;
     }
 
@@ -73,11 +74,10 @@ public abstract class Order {
     @Override
     public String toString() {
         return "Order{" +
-                "customID=" + customID +
-                ", items=" + items +
-                ", orderComplete=" + orderComplete +
-                ", time=" + time +
-                '}';
+                "customID " + customID +
+                " items " + Arrays.toString(items) +
+                " orderComplete " + orderComplete +
+                " time " + time +
+                " }";
     }
-
 }
