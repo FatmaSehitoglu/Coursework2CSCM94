1df003ca9e87f54c1776d38ba87e461d7c610d68
NzyZzz1998 <101812716+NzyZzz1998@users.noreply.github.com>
2022-03-30 12:34:15 +0100

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -10,17 +10,37 @@ public class FileManager {
     public static void writeToFile(String filename, String data) {
         try {
             FileWriter writer = new FileWriter(filename, true);
-            writer.write(data);
-            writer.close();
+            BufferedWriter bw = new BufferedWriter(writer);
+            bw.newLine();
+            bw.write(data);
+            bw.flush();
+            bw.close();
             System.out.println("Successfuly wrote " + data + " to " + filename);
         } catch (IOException e) {
             System.out.println("An error occurred");
         }
     }
 
-    
+
+    public static void readOrderHistory(String filename, String customerID) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        Scanner reader = new Scanner(br);
+        while (reader.hasNextLine()) {
+            list.add(reader.nextLine());
+        }
+        reader.close();
+        for (int i = 0; i < list.size(); i++) {
+            String[] split = list.get(i).split(" ");
+            if (split[1].equals(customerID)){
+                System.out.println(split[3]);
+                System.out.println(split[8]);
+            }
+        }
+    }
+
     public static String[][] readFromFile(String filename, int n) {
-        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<String> list = new ArrayList<>();
         try {
             File myObj = new File(filename);
             Scanner reader = new Scanner(myObj);
@@ -37,7 +57,7 @@ public class FileManager {
             //System.out.println("Line read: " + list.get(i));
         }
 
-        String[][] output = new String[list.size()][n]; 
+        String[][] output = new String[list.size()][n];
         //String[] temp = new String[n];
         for (int i = 0; i < list.size(); i++){
             String[] temp = list.get(i).split("\\|");
@@ -46,9 +66,6 @@ public class FileManager {
                 output[i][j] = temp[j];
             }
         }
-
         return output;
     }
-    
-
 }
