499fa97daa4ad34fda2ccdd08e63f07b2243e4ee
NzyZzz1998 <101812716+NzyZzz1998@users.noreply.github.com>
2022-04-03 16:45:55 +0100

    Update Database.java

--- Database.java
+++ Database.java
@@ -252,6 +252,18 @@ public class Database {
         }
         return null;
     }
+    
+     public static boolean romoveStaffFromDatabase(String ID) {
+
+        for (Staff a: staffList){
+            if (a.getId() == ID){
+                staffList.remove(a);
+                return true;
+            }
+        }
+        return false;
+
+    }
 
     //MENU ITEMS DATABASE
     //==== ===== ========
