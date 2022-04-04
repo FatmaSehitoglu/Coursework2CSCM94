042736cba421a1ce5b6d26bde73a5ce457b9b0ca
Chris Litting <mr_litting@hotmail.com>
2022-04-04 14:55:34 +0100

    Updated Javadoc comments for Takeaway

--- Takeaway.java
+++ Takeaway.java
@@ -1,29 +1,55 @@
-import java.io.File;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-
+/**
+ * <h1>Takeaway class</h1>
+ * A subclass of Order class
+ * @author Niu Zhaoye
+ * @version 1.0
+ */
 public class Takeaway extends Order{
+   
+    //FIELDS
+    //======
+
     private String arriveTime;
 
     //CONSTRUCTORS
     //============
 
-    public Takeaway(String customID, ArrayList<MenuItem> items, boolean approval, LocalDateTime time) {
+    /**
+     * Constructor for new Takeaway Orders
+     * @param customID  as a String
+     * @param items     as a MenuItem ArrayList
+     * @param approval  as a boolean
+     */
+    public Takeaway(
+        String customID, 
+        ArrayList<MenuItem> items, 
+        boolean approval
+    ) {
         super(customID, items, approval);
     }
 
     /**
      *
-     * @param customID
-     * @param items
-     * @param orderComplete
-     * @param time
-     * @param chefComplete
-     * @param approval
-     * @param arriveTime
+     * @param customID      as a String
+     * @param items         as a MenuItem ArrayList
+     * @param orderComplete as a boolean
+     * @param time          as a LocalDateTime
+     * @param chefComplete  as a boolean
+     * @param approval      as a boolean
+     * @param arriveTime    as a String
      */
-    public Takeaway(String customID, ArrayList<MenuItem> items, boolean orderComplete, LocalDateTime time, boolean chefComplete, boolean approval, String arriveTime) {
+    public Takeaway(String customID, 
+        ArrayList<MenuItem> items, 
+        boolean orderComplete, 
+        LocalDateTime time, 
+        boolean chefComplete, 
+        boolean approval, 
+        String arriveTime
+    ) {
         super(customID, items, approval);
         this.orderComplete = orderComplete;
         this.time = time;
@@ -33,6 +59,11 @@ public class Takeaway extends Order{
 
     //SETTERS
     //=======
+
+    /**
+     * Sets arriveTime
+     * @param arriveTime as a String
+     */
     public void setArriveTime(String arriveTime) {
         this.arriveTime = arriveTime;
         Database.refreshOrders();
@@ -40,24 +71,55 @@ public class Takeaway extends Order{
       
     //GETTERS
     //=======
+
+    /**
+     * Gets arriveTime
+     * @return arriveTime as a String
+     */
     public String getArriveTime() {
         return arriveTime;
     }
 
-   
+    //UTILITY METHODS
+    //======= =======
 
+    /**
+     * toString method for Takeaway Order (for menu displays)
+     * @return Takeaway as a String
+     */
     @Override
     public String toString() {
-        return "Takeaway{" +
-                "arriveTime " + arriveTime +
-                " items " + items.toString() +
-                "chefComplete " + chefComplete +
-                " orderComplete " + orderComplete +
-                " }";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String time1 = time.format(formatter);
+        String order1 = items.toString();
+        String status1 = orderComplete ? "Complete" : "Incomplete";
+        String status2 = chefComplete ? "Cooking complete" : "Cooking in progress";
+        return String.format(
+            "Customer: %s, Time: %s, Estimated arrival time: %s, \nOrder: %s, \nCooking status: %s, Order Status: %s", 
+            customID, 
+            time1,
+            arriveTime, 
+            order1,
+            status2, 
+            status1
+        );
     }
 
+    /**
+     * Converts Takeaway Order to a data String for Database storage
+     * @return Takeaway as a data String
+     */
     public String toDataString() {
-        return String.format("%s|%s|%b|%s|%b|%b|%s\n", customID, FileManager.getItemIds(items), orderComplete, FileManager.LocalDateTimeToString(time), chefComplete, approval, arriveTime);
+        return String.format(
+            "%s|%s|%b|%s|%b|%b|%s\n", 
+            customID, 
+            FileManager.getItemIds(items), 
+            orderComplete, 
+            FileManager.LocalDateTimeToString(time), 
+            chefComplete, 
+            approval, 
+            arriveTime
+        );
     }
 
 }
