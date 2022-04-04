3b2800ead1df43ae86245f40dc6a8fb00c63306f
Chris Litting <mr_litting@hotmail.com>
2022-04-04 19:14:32 +0100

    Minor change to FileManager

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Database"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"FileManager"}];updateSearchResults();
