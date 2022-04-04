bd8ff38b0a72710cd4e8640a53cf0ac2d8bca656
Chris Litting <mr_litting@hotmail.com>
2022-04-04 19:20:24 +0100

    Minor change to Waiter

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Order"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Waiter"}];updateSearchResults();
